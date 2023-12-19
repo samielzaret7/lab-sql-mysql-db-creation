3f4b729948ee0d7d0b40c73a47d3e33d62ebe1af
samielzaret7 <sz.zengotita@gmail.com>
2023-12-19 12:11:16 -0400

    Solved Lab

--- create.sql
+++ create.sql
@@ -5,12 +5,12 @@ USE lab_mysql;
 CREATE TABLE customers (
 customer_id int NOT NULL auto_increment,
 name varchar(20),
-phone_number char(12),
+phone_number varchar(25),
 email varchar(255),
-city varchar(20),
 address varchar(255),
-country varchar(60),
+city varchar(20),
 state varchar(105),
+country varchar(60),
 postal_code varchar(16),
 PRIMARY KEY (customer_id)
 );
@@ -18,7 +18,7 @@ PRIMARY KEY (customer_id)
 CREATE TABLE salespersons (
   staff_id int NOT NULL auto_increment,
   name varchar (20),
-  store char (4),
+  store varchar (60),
   PRIMARY KEY (staff_id)
 );
 
@@ -26,15 +26,15 @@ CREATE TABLE salespersons (
 CREATE TABLE cars (
   vin char(17),
   manufacturer varchar (30),
-  color varchar (20),
-  year smallint(4),
   model varchar(20),
+   year smallint(4),
+  color varchar (20),
   PRIMARY KEY (vin)
 );
 
 CREATE TABLE invoices (
   invoice_number int NOT NULL auto_increment,
-  vin char(17),
+  vin char,
   customer_id int,
   staff_id int,
   date datetime,
@@ -43,12 +43,3 @@ CREATE TABLE invoices (
   CONSTRAINT cust_ref FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
   CONSTRAINT staff_ref FOREIGN KEY (staff_id) REFERENCES salespersons (staff_id)
 );
-
-ALTER TABLE customers DROP COLUMN phone_number;
-ALTER TABLE customers ADD phone_number CHAR(25);
-
-ALTER TABLE customers CHANGE COLUMN phone_number phone_number CHAR(25) AFTER name;
-
-ALTER TABLE salespersons DROP COLUMN store;
-ALTER TABLE salespersons ADD store VARCHAR(30);
-
